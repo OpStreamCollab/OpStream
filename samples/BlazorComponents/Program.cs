cbeb0dd5e9827e1142a28d3bafee68cf2686b8f4
jvera71 <j_vera@hotmail.com>
2026-05-30 20:18:03 +0200

    Refactor 2

---
+++ samples/BlazorComponents/Program.cs
@@ -0,0 +1,36 @@
+using BlazorComponents.Components;
+
+namespace BlazorComponents
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddOpStreamClient()
+                   .UseSignalRTransport(options =>
+                   {
+                       options.HubUrl = "http://localhost:50109/collab";
+                   });
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
